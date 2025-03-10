a30a7a76c9b298a6d817cb0248bee96bc0deb551
2eve2 <ydajeong7@gmail.com>
2025-03-10 12:12:23 +0900

    Feat: props(2)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import Hello from './Hello';
 
 function App() {
   return (
-    <Hello name="다정" color="blue" />
+    <>
+      <Hello name="다정" color="blue" />
+      <Hello color="pink"/>
+    </>
   );
 }
 
--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-function Hello(props) {
-  return <div style={{color:props.color}}>안녕하세요 {props.name}</div>
+function Hello({color, name = '이름없음'}) {
+  return <div style={{color}}>안녕하세요 {name}</div>
 }
+
 
 export default Hello;
