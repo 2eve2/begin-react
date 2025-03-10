a80da6aebad049ebd0cc6946bfcfa0602efaf5e7
2eve2 <ydajeong7@gmail.com>
2025-03-10 12:15:05 +0900

    Feat: Wrapper-children

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Hello from './Hello';
+import Wrapper from './Wrapper';
 
 function App() {
   return (
-    <>
+    <Wrapper>
       <Hello name="다정" color="blue" />
       <Hello color="pink"/>
-    </>
+    </Wrapper>
   );
 }
 
