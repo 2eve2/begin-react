e5eda63b6f9ff08df7c64f2b117c1e031764bf0d
2eve2 <ydajeong7@gmail.com>
2025-03-10 15:04:04 +0900

    Feat: counter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
+import './App.css';
+import Counter from './components/Counter';
 
 function App() {
   return (
-    <Wrapper>
-      <Hello name="다정" color="blue" isSpecial/>
-      {/* props 값을 설정하게 될 때 값 설정을 생략한다면 ={true}와 같다. */}
-      <Hello color="pink"/>
-    </Wrapper>
+    <Counter/>
   );
 }
 
